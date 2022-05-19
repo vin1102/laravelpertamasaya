d229569887a225dc3dc0ab0567379cdcb353d918
vin1102 <kelvinfebriannugroho@gmail.com>
2022-05-19 09:20:41 +0700

    commit11

--- resources/views/Friends/show.blade.php
+++ resources/views/Friends/show.blade.php
@@ -8,6 +8,7 @@
         <h3>Nama Teman : {{ $friend['nama'] }}</h3>
         <h3>No Telepon Teman : {{$friend['no_telp']}}</h3>
         <h3>Alamat Teman : {{$friend['alamat']}}</h3>
+
         
     </div>
 </div>
